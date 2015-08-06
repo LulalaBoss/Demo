c41899a5aa874d15f3429174fa13c381d691adac
LulalaBoss <katyhatesnake@gmail.com>
2015-08-06 14:52:30 -0700

    writing a string

---
+++ Demo/Demo/Engine/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Demo.Models;
+
+namespace Demo.Engine
+{
+    public class GameState
+    {
+        private Store store;
+
+        public GameState(string name, int cash)
+        {
+            store = new Store(name, cash);
+
+        }
+
+        public int getStoreCash() { return store.cash; }
+        public string getStoreName() { return store.name; }
+    }
+}
---
+++ Demo/Demo/Models/Store.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo.Models
+{
+    public class Store
+    {
+        public string name;
+        public int cash;
+
+        public Store(string name, int cash)
+        {
+            this.name = name;
+            this.cash = cash;
+        }
+    }
+}
